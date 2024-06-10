01c7ba22299ee027d52d58a9bf061b2ba9ef793a
Karsten <karstenkleung@gmail.com>
2024-06-09 21:45:31 -0400

    Added router to get about, project, and contact page

--- ASSIGNMENT01/routes/index.js
+++ ASSIGNMENT01/routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('project', { title: 'Projects' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me' });
 });
 
 module.exports = router;
