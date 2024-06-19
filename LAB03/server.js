4938bff761b68b69aebe96abcaa25d25e79e0b3c
Karsten <karstenkleung@gmail.com>
2024-06-19 10:55:43 -0400

    Created calculate function

--- LAB03/server.js
+++ LAB03/server.js
@@ -1,6 +1,33 @@
 var connect = require("connect");
+var http = require('http');
 var url = require("url");
 
+var app = connect();
+
+app.use(function(req, res){
+    res.end(req.originalUrl);
+    res.end("Hello from connect");
+});
+
+http.createServer(app).listen(3000);
+
+// url.parse
+
 function calculate(operator, operandOne, operandTwo) {
-    
+    switch(operator) {
+        case "add":
+            return operandOne + operandTwo;
+            break;
+        case "subtract":
+            return operandOne - operandTwo;
+            break;
+        case "multiply":
+            return operandOne * operandTwo;
+            break;
+        case "divide":
+            return operandOne / operandTwo;
+            break;
+        default:
+            break;
+    }
 }
