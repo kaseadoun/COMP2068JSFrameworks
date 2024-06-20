f7d2e42fdfcb61f4ebf96885f999c3bf0a9999da
Karsten <karstenkleung@gmail.com>
2024-06-20 00:09:59 -0400

    Added comments

--- LAB03/server.js
+++ LAB03/server.js
@@ -1,28 +1,33 @@
+// Required packages
 var connect = require("connect");
 var http = require('http');
 var url = require("url");
-
+// Create instance of Connect
 var app = connect();
-
+// Middleware to display the expression
 app.use(function(req, res){
+    // Set a variable to the parsed URL object
     let urlObject = url.parse(req.originalUrl, true);
+    // If query exists in object...
     if ('query' in urlObject) {
+        // Set the values found in query, get the values and set it in its respective variables
         query = urlObject.query;
         let operator = query.method;
         let operandOne = query.x;
         let operandTwo = query.y;
-
+        // Calculate and save the answer
         let answer = calculate(operator, parseInt(operandOne), parseInt(operandTwo));
-
+        // Get the operator symbol
         let operatorSign = operatorSymbol(operator);
-
+        // Write it on the page
         res.write(`${operandOne} ${operatorSign} ${operandTwo} = ${answer}`);
     }
+    // End response process
     res.end();
 });
-
+// Server port
 http.createServer(app).listen(3000);
-
+// Function to calculate
 function calculate(operator, operandOne, operandTwo) {
     switch(operator) {
         case "add":
@@ -41,7 +46,7 @@ function calculate(operator, operandOne, operandTwo) {
             break;
     }
 }
-
+// Function to set the symbol
 function operatorSymbol(operator) {
     switch(operator) {
         case "add":
