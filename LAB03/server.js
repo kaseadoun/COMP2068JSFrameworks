18e242fc9ba618d78dd372525afee5697acd8a86
Karsten <karstenkleung@gmail.com>
2024-06-20 00:06:04 -0400

    Completed calculate, operatorSymbol, and app.use

--- LAB03/server.js
+++ LAB03/server.js
@@ -5,13 +5,23 @@ var url = require("url");
 var app = connect();
 
 app.use(function(req, res){
-    res.end(req.originalUrl);
-    res.end("Hello from connect");
+    let urlObject = url.parse(req.originalUrl, true);
+    if ('query' in urlObject) {
+        query = urlObject.query;
+        let operator = query.method;
+        let operandOne = query.x;
+        let operandTwo = query.y;
+
+        let answer = calculate(operator, parseInt(operandOne), parseInt(operandTwo));
+
+        let operatorSign = operatorSymbol(operator);
+
+        res.write(`${operandOne} ${operatorSign} ${operandTwo} = ${answer}`);
+    }
+    res.end();
 });
 
 http.createServer(app).listen(3000);
-
-// url.parse
 
 function calculate(operator, operandOne, operandTwo) {
     switch(operator) {
@@ -27,6 +37,21 @@ function calculate(operator, operandOne, operandTwo) {
         case "divide":
             return operandOne / operandTwo;
             break;
+        default:
+            break;
+    }
+}
+
+function operatorSymbol(operator) {
+    switch(operator) {
+        case "add":
+            return '+'
+        case "subtract":
+            return '-'
+        case "multiply":
+            return 'x'
+        case "divide":
+            return '/'
         default:
             break;
     }
