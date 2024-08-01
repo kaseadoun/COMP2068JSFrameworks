a0cf0a2b7fa9b019d89f9dfed99304b422f5d15f
Karsten <karstenkleung@gmail.com>
2024-08-01 17:51:59 -0400

    Made GET request for expense

--- ASSIGNMENT02/backend/routes/expenses.js
+++ ASSIGNMENT02/backend/routes/expenses.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
-
 const Expense = require("../models/expense");
-const config = require("../config/globals");
 
-router.get("/", (req, res, next) => {
-    res.render('index', { title: 'Expenses' });
+router.get("/", async (req, res, next) => {
+    let expenses = await Expense.find().sort([["date", "ascending"]]);
+    return res.status(200).json(expenses);
 })
 
 module.exports = router;
