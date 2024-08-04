f3d10e165c4f6b092c53962913b58a46cb0875a6
Karsten <karstenkleung@gmail.com>
2024-08-04 10:50:47 -0400

    Working on login and registration

--- ASSIGNMENT02/routes/index.js
+++ ASSIGNMENT02/routes/index.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
+var User = require("../models/user");
+var passport = require("passport");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Finance Tracker' });
+});
+
+// --------------------------------------------- Login
+router.get('/login', (req, res, next) => {
+  let messages = req.session.messages || [];
+  req.session.messages = [];
+  res.render("login", { title: "Login to Your Account", messages: messages});
+});
+
+router.post('/login', passport.authenticate("local", {
+  successRedirect: "/overview",
+  failureRedirect: "/login",
+  failureMessage: "Invalid Credentials",
+}));
+
+// --------------------------------------------- Register
+router.get("/register", (req, res, next) => {
+  res.render("register", { title: "Create a New Account" });
+});
+
+router.post("/register", (req, res, next) => {
+  User.register(
+    new User({ username: req.body.username }),
+    req.body.password,
+    (err, newUser) => {
+      if (err) {
+        return res.redirect("/register");
+      } else {
+        req.login(newUser, (err) => {
+          res.redirect("/overview");
+        });
+      }
+    }
+  );
+});
+
+// --------------------------------------------- Logout
+router.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    res.redirect("/login");
+  });
 });
 
 module.exports = router;
