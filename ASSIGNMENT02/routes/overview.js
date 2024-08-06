e9c4397ee77e82f9f6542d3282e777de9f452afb
Karsten <karstenkleung@gmail.com>
2024-08-06 19:40:37 -0400

    Routed sum of income and expense for each month for the bar chart

--- ASSIGNMENT02/routes/overview.js
+++ ASSIGNMENT02/routes/overview.js
@@ -10,14 +10,22 @@ router.get('/', AuthenticationMiddleware, (req, res, next) => {
     res.render('overview', {title: "Overview", user: req.user});
 });
 
+// GET income and puts the sum based on the month on the bar chart
 router.get('/income_data', AuthenticationMiddleware, async (req, res, next) => {
+    const months = {'Jan': 0, 'Feb': 0, 'Mar': 0, 'Apr': 0, 'May': 0, 'Jun': 0, 'Jul': 0, 'Aug': 0, 'Sep': 0, 'Oct': 0, 'Nov': 0, 'Dec': 0};
+
     try {
         let incomes = await Incomes.find({ user: req.user._id }).sort({ date: -1 });
 
+        incomes.forEach(income => {
+            const month = new Date(income.date.getMonth()).toLocaleString('default', { month: 'short' });
+            months[month] += income.amount;
+        });
+
         let chartData = {
-            labels: incomes.map(income => income.date.toISOString().split('T')[0]),
+            labels: Object.keys(months),
             datasets: [{
-                data: incomes.map(income => income.amount)
+                data: Object.values(months)
             }],
             user: req.user
         };
@@ -27,14 +35,22 @@ router.get('/income_data', AuthenticationMiddleware, async (req, res, next) => {
     }
 });
 
+// GET expense and puts the sum based on the month on the bar chart
 router.get('/expense_data', AuthenticationMiddleware, async (req, res, next) => {
+    const months = {'Jan': 0, 'Feb': 0, 'Mar': 0, 'Apr': 0, 'May': 0, 'Jun': 0, 'Jul': 0, 'Aug': 0, 'Sep': 0, 'Oct': 0, 'Nov': 0, 'Dec': 0};
+
     try {
         let expenses = await Expenses.find({ user: req.user._id }).sort({ date: -1 });
 
+        expenses.forEach(expense => {
+            let month = new Date(expense.date.getMonth()).toLocaleString('default', { month: 'short' });
+            months[month] += expense.amount;
+        });
+
         let chartData = {
-            labels: expenses.map(expense => expense.date.toISOString().split('T')[0]),
+            labels: Object.keys(months),
             datasets: [{
-                data: expenses.map(expense => expense.amount)
+                data: Object.values(months)
             }],
             user: req.user
         };
