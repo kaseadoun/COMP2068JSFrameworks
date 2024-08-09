9d273009c700dc15f13888e847b0cd315ca1ffed
Karsten <karstenkleung@gmail.com>
2024-08-09 14:40:47 -0400

    Fixed read only chart in index

--- ASSIGNMENT02/public/js/indexChart.js
+++ ASSIGNMENT02/public/js/indexChart.js
@@ -1,29 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetchData();
+    fetchIncomeAndExpenseData()
 });
 
-async function fetchData() {
-    try {
-        const response = await fetch('/');
-        const data = await response.json();
-        
-        const months = Object.keys(data);
-        const incomeData = months.map(month => data[month][0]);
-        const expenseData = months.map(month => data[month][1]);
+async function fetchIncomeAndExpenseData() {
+  try {
+    const response = await fetch("/index/sample_data");
+    const data = await response.json();
 
-        renderChart('sampleIncomeChart', 'Income', months, incomeData);
-        renderChart('sampleExpenseChart', 'Expenses', months, expenseData);
-    } catch (err) {
-        console.error('Error fetching data:', error);
+    let incomeData = [];
+    let expenseData = []
+
+    for (let i = 0; i < 12; i++) {
+        incomeData.push(data.datasets[0].data[i][0]);
+        expenseData.push(data.datasets[0].data[i][1]);
     }
+
+    renderChart("sampleIncomeChart", "Income", data.labels, incomeData);
+    renderChart("sampleExpenseChart", "Expenses", data.labels, expenseData);
+  } catch (err) {
+    console.error("Error fetching data:", err);
+  }
 }
 
-function renderChart(chartId, label, xAxisLabel, data) {
-    const ctx = document.getElementById(chartId).getContext('2d');
+function renderChart(chartId, label, xAxisLabels, data) {
+    let ctx = document.getElementById(chartId).getContext("2d");
     new Chart(ctx, {
-        type: 'bar',
+        type: "bar",
         data: {
-            labels: labels,
+            labels: xAxisLabels,
             datasets: [{
                 label: label,
                 data: data,
--- ASSIGNMENT02/routes/index.js
+++ ASSIGNMENT02/routes/index.js
@@ -5,27 +5,32 @@ var Sample = require("../models/sample");
 var passport = require("passport");
 
 /* GET home page. */
-router.get("/", async (req, res, next) => {
+router.get("/", (req, res, next) => {
+  res.render("index", { 
+    title: "Finance Tracker"
+  });
+});
+
+router.get("/index/sample_data", async (req, res, next) => {
   const months = {
-    Jan: [0,0],
-    Feb: [0,0],
-    Mar: [0,0],
-    Apr: [0,0],
-    May: [0,0],
-    Jun: [0,0],
-    Jul: [0,0],
-    Aug: [0,0],
-    Sep: [0,0],
-    Oct: [0,0],
-    Nov: [0,0],
-    Dec: [0,0],
+    'Jan': [0,0],
+    'Feb': [0,0],
+    'Mar': [0,0],
+    'Apr': [0,0],
+    'May': [0,0],
+    'Jun': [0,0],
+    'Jul': [0,0],
+    'Aug': [0,0],
+    'Sep': [0,0],
+    'Oct': [0,0],
+    'Nov': [0,0],
+    'Dec': [0,0],
   };
 
   let samples = await Sample.find();
 
   samples.forEach(sample => {
     for (const [key, value] of Object.entries(months)) {
-      // Extract the index of the current month (Jan = 0, Feb = 1, etc.)
       const monthIndex = Object.keys(months).indexOf(key);
 
       // Safeguard against undefined values in income and expense arrays
@@ -37,32 +42,18 @@ router.get("/", async (req, res, next) => {
       }
     }
   });
-  // console.log(samples)
 
-  // for (const [key, value] of Object.entries(months)) {
-  //   months[key][0] = samples.income[0]
-  //   months[key][1] = samples.expense[0]
-  // }
+  let chartData = {
+    labels: Object.keys(months),
+    datasets: [{
+      data: Object.values(months)
+    }]
+  };
 
-  samples.forEach(sample => {
-    // sample.income.forEach((income, i) => {
-    //   const month = Object.keys(months)[i];
-    //   months[month][0] += income;
-    // });
-    // sample.expense.forEach((expense, i) => {
-    //   const month = Object.keys(months)[i];
-    //   months[month][1] += expense;
-    // });
-    console.log(sample);
-  });
+  console.log(Object.values(months))
 
-  // res.json(months);
-  res.render("index", { 
-    title: "Finance Tracker", 
-    user: req.user,
-    months: months
-  });
-});
+  res.json(chartData);
+})
 
 // --------------------------------------------- Login
 router.get("/login", (req, res, next) => {
@@ -71,7 +62,6 @@ router.get("/login", (req, res, next) => {
   res.render("login", {
     title: "Login to Your Account",
     messages: messages,
-    user: req.user,
   });
 });
 
@@ -86,7 +76,7 @@ router.post(
 
 // --------------------------------------------- Register
 router.get("/register", (req, res, next) => {
-  res.render("register", { title: "Create a New Account", user: req.user });
+  res.render("register", { title: "Create a New Account" });
 });
 
 router.post("/register", (req, res, next) => {
